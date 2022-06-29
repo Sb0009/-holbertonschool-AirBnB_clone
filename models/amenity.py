f8ef173984ae0634c3dabb6a149c9b5aa9c38608
siham <phenissia2009@hotmail.fr>
2022-06-29 10:00:27 +0200

    amenity file models

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,6 @@
 
 #!/usr/bin/python3
+# task 9
 
 """create the class"""
 
@@ -11,3 +12,10 @@ class Amenity(BaseModel):
     """create the class"""
 
     name = ''
+    city_id = 'City.id'
+    user_id = 'User.id'
+    description = ''
+    amenity_ids = 'Amenity.id'
+    place_id = 'Place.id'
+    user_id = 'User.id'
+    text = ''
